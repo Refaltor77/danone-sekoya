f0e264413429347f523cb35a094f04b9ed895564
Elysio Martins <elysiomartinspro@gmail.com>
2024-09-13 14:32:22 +0200

    feat: add footer ticket

--- resources/js/Pages/Sections/Footer.jsx
+++ resources/js/Pages/Sections/Footer.jsx
@@ -16,10 +16,21 @@ export default function Footer() {
                 alt={"second-ladybird"}
                 id={"ladyfooter"}
             />
-            <div className="p-6 sm:p-10 lg:px-32 text-white flex items-center justify-between flex-wrap">
+            <div className={"p-6 sm:p-5 lg:px-32 flex flex-col gap-0 sm:gap-0"}>
+                <p className="rubik text_nav_footer">¹ La composition des paniers gourmands est détaillée dans le <a
+                    className={"underline"} href={"/storage/rules.pdf"}>règlement du jeu.</a></p>
+                <span className="rubik text_nav_footer mt-4">Jeu sans obligation d'achat du 16/09/2024 au 16/10/2024 réservé aux personnes physiques majeures
+                    résidant en France (Corse comprise). Voir détails du <a className={"underline"}
+                                                                            href={"/storage/rules.pdf"}>règlement du jeu ici.</a></span>
+                <span className="rubik text_nav_footer mt-4">² Blédina SAS, RCS de Lyon 301 374 922</span>
+                <span className="rubik text_nav_footer">³ RIBAMBEL SA, RCS de Nanterre 538 929 076</span>
+                <span className="rubik text_nav_footer">BOUGER, JOUER EST INDISPENSABLE AU DÉVELOPPEMENT DE VOTRE ENFANT</span>
+            </div>
+            <div className="p-6 sm:p-5 lg:px-32 text-white flex items-center justify-between flex-wrap">
                 {/* Navigation links */}
                 <nav className="flex items-center gap-2 flex-wrap">
-                    <a className="rubik text_nav_footer" href={"/storage/rules.pdf"} target={"_blank"}>Règlement et conditions¹</a>
+                    <a className="rubik text_nav_footer" href={"/storage/rules.pdf"} target={"_blank"}>Règlement et
+                        conditions¹</a>
                     <p className="rubik text_nav_footer">|</p>
                     <a className="rubik text_nav_footer"
                        href={"https://shop.bledina.com/content/13-politique-de-protection-de-la-vie-privee"}
@@ -37,14 +48,6 @@ export default function Footer() {
                         className="mt-10 lg:mt-0"
                     />
                 </a>
-            </div>
-            <div className={"p-6 sm:p-10 lg:px-32 flex flex-col gap-0 sm:gap-0"}>
-                <p className="rubik text_nav_footer">¹ La composition des paniers gourmands est détaillée dans le <a className={"underline"} href={"/storage/rules.pdf"}>règlement du jeu.</a></p>
-                <span className="rubik text_nav_footer mt-4">Jeu sans obligation d'achat du 16/09/2024 au 16/10/2024 réservé aux personnes physiques majeures
-                    résidant en France (Corse comprise). Voir détails du <a className={"underline"} href={"/storage/rules.pdf"}>règlement du jeu ici.</a></span>
-                <span className="rubik text_nav_footer mt-4">² Blédina SAS, RCS de Lyon 301 374 922</span>
-                <span className="rubik text_nav_footer">³ RIBAMBEL SA, RCS de Nanterre 538 929 076</span>
-                <span className="rubik text_nav_footer">"BOUGER, JOUER EST INDISPENSABLE AU DÉVELOPPEMENT DE VOTRE ENFANT"</span>
             </div>
         </footer>
     );
